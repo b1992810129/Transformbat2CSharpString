eab996e742dc9b1533bf28dbe4012be6b4a8f7ef
hjhuang <Howard.Jhuang@hermes-microvision.com>
2019-09-23 18:27:27 +0800

    處理\與*的狀況

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Transformbat2CSharpString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(args[0]);
+            StreamWriter sw=new StreamWriter(Path.GetFileNameWithoutExtension(args[0])+"_.txt");
+            while (sr.EndOfStream == false)
+            {
+                string str = sr.ReadLine().Replace("\\", "\\\\");
+                str="\""+str.Replace("\"","\\\"") + "\\n\""+"+";
+                sw.WriteLine(str);
+            }
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
